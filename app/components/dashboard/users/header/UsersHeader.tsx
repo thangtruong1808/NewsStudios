82d86f357927d678fcac8db31f90b6052c458a6d
Thang Truong <thangtruong1808@gmail.com>
2025-06-07 12:42:29 +1000

    Removed Create User button for non-admin users login

--- app/components/dashboard/users/header/UsersHeader.tsx
+++ app/components/dashboard/users/header/UsersHeader.tsx
@@ -2,8 +2,12 @@
 
 import Link from "next/link";
 import { PlusIcon } from "@heroicons/react/24/outline";
+import { useSession } from "next-auth/react";
 
 export default function UsersHeader() {
+  const { data: session } = useSession();
+  const isAdmin = session?.user?.role === "admin";
+
   return (
     <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6">
       <div>
@@ -14,13 +18,15 @@ export default function UsersHeader() {
         </p>
       </div>
 
-      <Link
-        href="/dashboard/users/create"
-        className="inline-flex h-10 items-center gap-2 rounded-md bg-gradient-to-r from-blue-600 to-blue-400 px-5 py-2 text-sm font-medium text-white transition-colors hover:from-blue-700 hover:to-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 justify-center items-center"
-      >
-        <PlusIcon className="h-5 mr-2" />
-        <span>Create User</span>
-      </Link>
+      {isAdmin && (
+        <Link
+          href="/dashboard/users/create"
+          className="inline-flex h-10 items-center gap-2 rounded-md bg-gradient-to-r from-blue-600 to-blue-400 px-5 py-2 text-sm font-medium text-white transition-colors hover:from-blue-700 hover:to-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 justify-center items-center"
+        >
+          <PlusIcon className="h-5 mr-2" />
+          <span>Create User</span>
+        </Link>
+      )}
     </div>
   );
 }
